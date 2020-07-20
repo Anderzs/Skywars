493973145928811a40242908ca392576e43e901e
Anderzs <andersballeby42@gmail.com>
2020-07-20 12:40:45 +0200

    Initial Commit

---
+++ src/dk/legendebente/skywars/Skywars.java
@@ -0,0 +1,12 @@
+package dk.legendebente.skywars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Skywars extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+}
